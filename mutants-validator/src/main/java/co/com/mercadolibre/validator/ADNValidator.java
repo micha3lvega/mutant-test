afb3f918d68dcca172ff4ce76b5a688c96b0f489
Michael Vega <micha3lvega@gmail.com>
2022-03-12 19:03:17 -0500

    Creacion de la clase ADNValidator
    Clase encarga de las validación de la cadena de ADN.

---
+++ mutants-validator/src/main/java/co/com/mercadolibre/validator/ADNValidator.java
@@ -0,0 +1,25 @@
+/**
+ *
+ */
+package co.com.mercadolibre.validator;
+
+/**
+ * Clase encargada de la validacion de una cadena de ADN
+ *
+ * @author Michael Vega
+ *
+ */
+public class ADNValidator {
+
+	/**
+	 * Metodo encargado de validar si un adn tiene una mutacion o no
+	 *
+	 * @param dna
+	 * @return true si la sequencia cumple con las reglas generadas
+	 */
+	public static boolean isMutant(String[] dna) {
+
+		return false;
+	}
+
+}
