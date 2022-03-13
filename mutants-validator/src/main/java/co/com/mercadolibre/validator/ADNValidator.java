fd11e3096223c2995bb324aab94e4b7030a87936
Michael Vega <micha3lvega@gmail.com>
2022-03-12 19:05:06 -0500

    Agregar método validateLength
    Creacion del método encargado de validar el tamaño de las filas y columnas

--- mutants-validator/src/main/java/co/com/mercadolibre/validator/ADNValidator.java
+++ mutants-validator/src/main/java/co/com/mercadolibre/validator/ADNValidator.java
@@ -3,6 +3,8 @@
  */
 package co.com.mercadolibre.validator;
 
+import co.com.mercado.libre.commons.dto.InvalidLengthException;
+
 /**
  * Clase encargada de la validacion de una cadena de ADN
  *
@@ -19,7 +21,30 @@ public class ADNValidator {
 	 */
 	public static boolean isMutant(String[] dna) {
 
+		// Validar tamano de las filas y columnas
+		validateLength(dna);
+
 		return false;
+	}
+
+	/**
+	 * Metodo que valida que la cadena sea de nXn
+	 *
+	 * @param dna
+	 */
+	private static void validateLength(String[] dna) {
+
+		// Obtener el tamano de la cadena
+		final var arrayLength = dna.length;
+
+		// Recorrer la cadena comparando el tamaño de cada fila
+		for (final String base : dna) {
+
+			if (base.length() != arrayLength)
+				throw new InvalidLengthException();
+
+		}
+
 	}
 
 }
