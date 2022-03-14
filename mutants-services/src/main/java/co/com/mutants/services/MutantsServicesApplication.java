313d477e6282766e53243becff44f571b6630515
Michael Vega <micha3lvega@gmail.com>
2022-03-14 14:41:27 -0500

    Creacion del proyecto mutants-controller
    Proyecto encargado de exponer los servicios rest para la validación de si un humano es o no mutante

---
+++ mutants-services/src/main/java/co/com/mutants/services/MutantsServicesApplication.java
@@ -0,0 +1,13 @@
+package co.com.mutants.services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MutantsServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MutantsServicesApplication.class, args);
+	}
+
+}
