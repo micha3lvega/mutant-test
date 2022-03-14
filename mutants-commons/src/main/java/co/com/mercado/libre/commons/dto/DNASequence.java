1fcadd8ca3e7fe256c06756e49ec0d113a484df7
Michael Vega <micha3lvega@gmail.com>
2022-03-14 16:43:41 -0500

    Modificar tipo del adn

--- mutants-commons/src/main/java/co/com/mercado/libre/commons/dto/DNASequence.java
+++ mutants-commons/src/main/java/co/com/mercado/libre/commons/dto/DNASequence.java
@@ -4,7 +4,6 @@
 package co.com.mercado.libre.commons.dto;
 
 import java.io.Serializable;
-import java.util.List;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -20,9 +19,8 @@ import lombok.NoArgsConstructor;
 public class DNASequence implements Serializable {
 
 	public static final String ALLOW_VALUES = "ATCG";
-
 	public static final String[] MUTATIONS = { "AAAA", "TTTT", "CCCC", "GGGG" };
 
-	private List<String> dna;
+	private String[] dna;
 
 }
