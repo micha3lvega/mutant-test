bd555cbad051c5c551176adb6858b8417eef9e1e
Michael Vega <micha3lvega@gmail.com>
2022-03-15 13:07:07 -0500

    Creacion del proyecto
    El proyecto mutants-persistence es el encargado de manejar el acceso a los datos del proyecto

---
+++ mutants-persistence/src/test/java/co/com/mutants/persistence/MutantsPersistenceApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.mutants.persistence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MutantsPersistenceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
