b526c12d54cb60461997fce0dd52d07e0e80d2ee
Michael Vega <micha3lvega@gmail.com>
2022-03-19 16:07:28 -0500

    Crear método findByMutant
    Método encargado de obtener las cadenas de ADN dependiendo si tienen o no una mutación

--- mutants-persistence/src/main/java/co/com/mutants/persistence/repository/ValidatedDNARepository.java
+++ mutants-persistence/src/main/java/co/com/mutants/persistence/repository/ValidatedDNARepository.java
@@ -1,5 +1,7 @@
 package co.com.mutants.persistence.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import co.com.mutants.persistence.entity.ValidatedDNA;
@@ -7,5 +9,7 @@ import co.com.mutants.persistence.entity.ValidatedDNA;
 public interface ValidatedDNARepository extends MongoRepository<ValidatedDNA, String> {
 
 	ValidatedDNA findByHash(String hash);
+
+	List<ValidatedDNA> findByMutant(Boolean isMutant);
 
 }
