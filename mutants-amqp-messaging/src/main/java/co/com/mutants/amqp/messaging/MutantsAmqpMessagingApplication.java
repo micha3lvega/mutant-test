80ded7bb1eaf90061a6dec72d58abab71e54b4ac
Michael Vega <micha3lvega@gmail.com>
2022-03-21 14:06:06 -0500

    Creacion del proyecto mutants-amqp-messaging
    Proyecto para la administración de la cola.

---
+++ mutants-amqp-messaging/src/main/java/co/com/mutants/amqp/messaging/MutantsAmqpMessagingApplication.java
@@ -0,0 +1,13 @@
+package co.com.mutants.amqp.messaging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MutantsAmqpMessagingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MutantsAmqpMessagingApplication.class, args);
+	}
+
+}
