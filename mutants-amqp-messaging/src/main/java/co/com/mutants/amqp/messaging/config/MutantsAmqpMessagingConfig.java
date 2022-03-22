b6c1262298bcd1f381c82185bbca395e3a99320b
Michael Vega <micha3lvega@gmail.com>
2022-03-22 13:39:18 -0500

    Activar llamados asíncronos además de los reintentos

--- mutants-amqp-messaging/src/main/java/co/com/mutants/amqp/messaging/config/MutantsAmqpMessagingConfig.java
+++ mutants-amqp-messaging/src/main/java/co/com/mutants/amqp/messaging/config/MutantsAmqpMessagingConfig.java
@@ -2,8 +2,12 @@ package co.com.mutants.amqp.messaging.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.retry.annotation.EnableRetry;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.web.client.RestTemplate;
 
+@EnableRetry
+@EnableAsync
 @Configuration
 public class MutantsAmqpMessagingConfig {
 
